e456e64299d6dadffd906044f7209bb815d54fa4
Jhonattan Florez <Jdflorez038@misena.edu.co>
2022-11-21 21:16:00 +0100

    Nuevas animaciones

--- js/three.js
+++ js/three.js
@@ -1,8 +1,3 @@
-/**
- * @license
- * Copyright 2010-2022 Three.js Authors
- * SPDX-License-Identifier: MIT
- */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
