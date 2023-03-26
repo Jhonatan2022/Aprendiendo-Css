c8c06cadf1da5466fb7d854bc1fcfa8b341a3f93
Jhonattan Florez <Jdflorez038@misena.edu.co>
2023-03-27 00:58:04 +0100

    Usando api de rick and morty

--- js/animaciones.js
+++
@@ -1,1 +0,0 @@
-// animación 
---
+++ js/api rick and morty.js
@@ -0,0 +1,61 @@
+// Api de rick and morty
+
+// Funcion para obtener los datos de la api
+const apiRick = async (pagina) => {
+  // Url de la api con el parametro de la pagina
+  let url = `https://rickandmortyapi.com/api/character/?page=` + pagina;
+  let url2 = 'https://rickandmortyapi.com/api/location/';
+
+  // Obtener los datos de la api
+  const api = await fetch(url);
+  const api2 = await fetch(url2);
+
+  // Convertir los datos a json
+  const data = await api.json();
+  const data2 = await api2.json();
+
+  // Imprimir los datos en consola
+  console.log(data, data2);
+
+  // Imprimir los datos en cada elemento del html
+  divRes = document.querySelector('#resultado');
+
+  // Realizamos el cambia de pagina con el boton
+  divRes.innerHTML = ""
+
+  // Imprimir los datos en el html
+  data.results.map(item => {
+    // Crear un div para cada personaje
+    divItem = document.createElement('div')
+
+    // Agregarle la clase item e insertar el div en el html
+    divItem.innerHTML = `
+
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="${item.image}" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title"><i class="fa-solid fa-signature" ></i>--> ${item.name}</h5>
+        <p class="card-text"><i class="fa-solid fa-dna" ></i>--> ${item.species}  </p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item"><i class="fa-solid fa-hashtag" ></i> Especimen: ${item.id}</li> 
+        <li class="list-group-item"><i class="fa-solid fa-clock-rotate-left" ></i> Estado del especimen: ${item.status}</li>
+        <li class="list-group-item"><i class="fa-solid fa-rocket" ></i> ${item.type}</li>
+      </ul>
+      <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div> 
+    `
+    // Insertar el div en el html por cada personaje que vaya recorriendo
+    divRes.appendChild(divItem);
+   
+  });
+};
+
+
+
+
+// Funcion para imprimir los datos de la api
+apiRick(1);
